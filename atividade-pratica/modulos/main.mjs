56e7939862e420cbeeffb3eea1966ef49debcc80
SALETE DOS SANTOS <salete_sm@hotmail.com>
2021-11-16 18:29:23 -0300

    atividade modulos

---
+++ atividade-pratica/modulos/main.mjs
@@ -0,0 +1,6 @@
+import { mostraIdade, mostraCidade, mostraHobby
+} from "./funcoes.mjs";
+
+console.log(mostraIdade("Joana", 25));
+console.log(mostraCidade("Marcos", "Rio de Janeiro"));
+console.log(mostraHobby("Marina", "Ler livros"));
